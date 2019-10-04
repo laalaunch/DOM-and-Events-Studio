eeae97435cb3fdba87bdab497122e4424c3c50a1
Lisa A <lastembo@gmail.com>
2019-10-04 14:26:09 -0500

    completed studio 14 requirements step 2

--- scripts.js
+++ scripts.js
@@ -1,2 +1,13 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+function takeOff() {
+    let shouldRun = window.confirm("Do you want to go through with this?")
+if (shouldRun) {
+    document.getElementById("takeoff").innerHTML;
+    document.getElementById("flightStatus").innerHTML = "Shuttle in Flight";
+    document.getElementById("shuttleBackground").style.backgroundColor = "blue";
+    document.getElementById("spaceShuttleHeight").innerHTML = 10000;
+}
+
+    
+}
