041eeb41029a9083dab37bf4a70ab42fa0ffa3d3
Lisa A <lastembo@gmail.com>
2019-10-04 14:42:49 -0500

    completed studio 14 requirements step 3

--- scripts.js
+++ scripts.js
@@ -2,12 +2,16 @@
 // Remember to pay attention to page loading!
 function takeOff() {
     let shouldRun = window.confirm("Do you want to go through with this?")
-if (shouldRun) {
-    document.getElementById("takeoff").innerHTML;
-    document.getElementById("flightStatus").innerHTML = "Shuttle in Flight";
-    document.getElementById("shuttleBackground").style.backgroundColor = "blue";
-    document.getElementById("spaceShuttleHeight").innerHTML = 10000;
+    if (shouldRun) {
+        document.getElementById("takeoff").innerHTML;
+        document.getElementById("flightStatus").innerHTML = "Shuttle in Flight";
+        document.getElementById("shuttleBackground").style.backgroundColor = "blue";
+        document.getElementById("spaceShuttleHeight").innerHTML = 10000;
+    }
 }
-
-    
+function land() {
+    window.alert("The shuttle is landing. Landing gear engaged.");
+    document.getElementById("flightStatus").innerHTML = "The shuttle has landed.";
+    document.getElementById("shuttleBackground").style.backgroundColor = "green";
+    document.getElementById("spaceShuttleHeight").innerHTML = 0;
 }
